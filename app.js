6d2a24ad428fb9d27667010a9ba8834bb75c86da
Shenhaoyi <shenhaoyi04@163.com>
2024-05-08 19:24:47 +0800

    Serving static files

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import path from 'node:path';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use(
+  express.static(path.join(process.cwd(), 'public'), {
+    maxAge: 60 * 1000, // 单位为毫秒
+  }),
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
