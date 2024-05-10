bd97713e86830ebf013a0cc246298d91b9c045af
Shenhaoyi <shenhaoyi04@163.com>
2024-05-10 12:38:52 +0800

    use mongoose to connect mongodb

---
+++ mongodb/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import chalk from 'chalk';
+
+/*
+  连接 mongodb 服务
+    默认端口可以省略
+    test 为数据库名
+*/
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+mongoose.connection.once('open', () => {
+  console.log(chalk.green('MongoDb connected'));
+  // 这里写入连接成功后的代码
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error(chalk.red('Error in MongoDb connection: ' + error));
+});
+
+mongoose.connection.on('close', () => {
+  console.log(chalk.green('MongoDb connection closed'));
+});
