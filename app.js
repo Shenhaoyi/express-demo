5995b3ecb87ceff762c952e11d44ea55cc784a93
Shenhaoyi <shenhaoyi04@163.com>
2024-05-12 15:13:28 +0800

    configuring expression-session

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ import connect from './mongodb/db.js';
 import book from './routes/api/book.js';
 import cookie from './routes/cookie.js';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
 const app = express();
 const port = 3000;
@@ -36,6 +38,22 @@ app.use('/ejs', ejs);
 app.use('/upload', upload);
 app.use('/book', book);
 app.use('/cookie', cookie);
+app.use(
+  session({
+    name: 'sid', // 设置 cookie 的 name，默认值是：connect.sid
+    secret: 'atguigu', // 参与加密的字符串（又称签名），又称为加盐
+    saveUninitialized: false, // 是否为每次请求都设置一个 cookie 用来存储 session 的 id。给匿名用户记录一些信息时可以使用
+    resave: true, // 是否在每次请求时重新保存session
+    // 将 session 保存到数据库
+    store: MongoStore.create({
+      mongoUrl: dbUrl,
+    }),
+    cookie: {
+      httpOnly: true, // 仅服务端可操作 cookie
+      maxAge: 1000 * 300, // 控制 sessionID 的过期时间，同时也控制 cookie
+    },
+  }),
+);
 
 app.use(
   express.static(path.join(process.cwd(), 'public'), {
@@ -44,7 +62,7 @@ app.use(
 );
 
 async function start() {
-  // await connect(dbUrl);
+  await connect(dbUrl);
   app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
   });
